4df19609859b1b8d202c984f46508d1a9f77bea5
SergePauli <SergePauli@yandex.ru>
2021-07-24 20:26:46 +0800

    feature:(wrap App in store  Context.Provider), stable

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Store from './store/store';
+interface IState{
+  store: Store
+}
+const store = new Store()
+export const Context = createContext<IState>({store}) 
 ReactDOM.render(  
+  <Context.Provider value={{store}} >
     <App />,  
+  </Context.Provider> , 
   document.getElementById('root')
 );
 
