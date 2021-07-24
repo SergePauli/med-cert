a4f45f7345c7e6477a8aaa34260d52cdf342c8a3
SergePauli <SergePauli@yandex.ru>
2021-07-24 17:05:54 +0800

    refactor:(change registration in AuthService), stable

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,12 +1,13 @@
 import $api from "../http"
 import { AxiosResponse } from "axios"
 import { AuthResponse } from "../models/responses/AuthResponse"
+import { IRegistration } from "../models/requests/IRegistration"
 export default class AuthService {
   static async login(email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
     return $api.post("auth/login", { email, password })
   }
-  static async registration(email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
-    return $api.post("auth/registration", { email, password })
+  static async registration(user: IRegistration): Promise<AxiosResponse<AuthResponse>> {
+    return $api.post("auth/registration", { user })
   }
   static async logout(): Promise<void> {
     return $api.post("auth/logout")
