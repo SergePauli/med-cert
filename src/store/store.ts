ba4ab5834822614f737a23fa6307814ea3a98107
SergePauli <SergePauli@yandex.ru>
2021-07-24 17:13:26 +0800

    feature:(add registration method to store), stable

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { makeAutoObservable } from "mobx"
 import { IUser } from "../models/IUser"
+import { IRegistration } from "../models/requests/IRegistration"
 import AuthService from "../services/AuthService"
 
 export default class Store {
@@ -20,6 +21,14 @@ export default class Store {
       localStorage.setItem("token", response.data.accessToken)
       this.setAuth(true)
       this.setUser(response.data.user)
+    } catch (e) {
+      console.log(e.response?.data?.message)
+    }
+  }
+  async registration(user: IRegistration) {
+    try {
+      const response = await AuthService.registration(user)
+      localStorage.setItem("token", response.data.accessToken)
     } catch (e) {
       console.log(e.response?.data?.message)
     }
