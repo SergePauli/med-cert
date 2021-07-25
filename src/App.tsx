3c99dfeb341e479f6bf2f9fb20ed7c74d1450e60
SergePauli <SergePauli@yandex.ru>
2021-07-25 10:10:04 +0800

    feature:(add useContext and useEffect to App), stable

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { observer } from 'mobx-react-lite'
 import React, {FC} from 'react'
+import { useContext } from 'react'
+import { useEffect } from 'react'
+import { Context } from '.'
 import { LoginPage } from './components/LoginPage'
 
-
 const App: FC =() => {
+  const {store} = useContext(Context)
+  useEffect(()=>{
+    if (localStorage.getItem('token')) {
+      store.checkAuth()
+    }
+  },[])
   return (
     <div>
+      <h1>{store.isAuth ? `Авторизован ${store.user.email}`: 'Авторизуйтесь'}</h1>
       <LoginPage />
     </div>
   )
 }
 
-export default App
+export default observer(App)
