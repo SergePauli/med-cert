abeb75c311250b330eaa3886b6d294b153821752
SergePauli <SergePauli@yandex.ru>
2021-07-25 17:57:58 +0800

    feature:(add interceptor.request to http/index), stable

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios"
+import { AuthResponse } from "../models/responses/AuthResponse"
 export const API_URL = "http://localhost:5000/REST_API/v1/"
 const $api = axios.create({
   withCredentials: true,
@@ -9,4 +10,22 @@ $api.interceptors.request.use((config: AxiosRequestConfig) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`
   return config
 })
+
+$api.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+      originalRequest._isRetry = true
+      try {
+        const response = await axios.get<AuthResponse>(`${API_URL}auth/refresh`, { withCredentials: true })
+        localStorage.setItem("token", response.data.tokens.access)
+        return $api.request(originalRequest)
+      } catch (e) {
+        console.log("не авторизован")
+      }
+    }
+    throw error
+  }
+)
 export default $api
