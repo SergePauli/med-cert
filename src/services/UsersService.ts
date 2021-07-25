45e9dff5899614208aeab0620ee84b6e1af3369c
SergePauli <SergePauli@yandex.ru>
2021-07-25 15:44:16 +0800

    fix:(post rout in fecthUsers),stable

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,9 +1,9 @@
-import $api from "../http"
+import $api, { API_URL } from "../http"
 import { AxiosResponse } from "axios"
 import { IUser } from "../models/IUser"
 export default class UsersService {
   static async fetchUsers(): Promise<AxiosResponse<IUser[]>> {
-    return $api.post("model/user/", {
+    return $api.post(`${API_URL}model/user/`, {
       render_options: { only: ["id", "email", "roles", "organization_id", "activated"] },
     })
   }
