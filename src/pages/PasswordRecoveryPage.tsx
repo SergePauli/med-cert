e607dba0dc61c145c542f8cc8050bd39c29e2a57
SergePauli <SergePauli@yandex.ru>
2021-07-26 16:05:01 +0800

    feature:(create PasswordRecoveryPage in pages), stable

---
+++ src/pages/PasswordRecoveryPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FC } from 'react';
+
+type PasswordRecoveryPageProps = {code:string}
+
+export const PassordRecoveryPage: FC<PasswordRecoveryPageProps> = (props: PasswordRecoveryPageProps) => (
+  <>
+    <h1>Страница замены пароля</h1>
+    <div>List</div>
+  </>
+);
