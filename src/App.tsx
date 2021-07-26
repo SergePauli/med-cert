c58992db506210aa1d624e22a47b0deb7dae50fc
SergePauli <SergePauli@yandex.ru>
2021-07-26 16:11:33 +0800

    refactor:(remove all temporaryes and add router in App), stable

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,22 @@ import React, {FC, useState} from 'react'
 import { useContext } from 'react'
 import { useEffect } from 'react'
 import { Context } from '.'
-import LoginPage from './components/LoginPage'
-import { IUser } from './models/IUser'
-import UsersService from './services/UsersService'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './components/AppRouter'
 
 const App: FC =() => {
-  const {store} = useContext(Context)
-  const [users, setUsers] = useState<IUser[]>([])
+  const {store} = useContext(Context)  
   useEffect(()=>{
     if (localStorage.getItem('token')) {
       store.checkAuth()
     }
-  },[store])
-    
-  const getUsers = async ()=>{
-    try {
-      const response = await UsersService.fetchUsers()      
-      if (response.data) setUsers(response.data) 
-      else setUsers([])
-    } catch (e) {
-      console.log(e.response?.data?.message)
-    }
-  }
-  
-  if (store.isLoding) {
-    return (<div>Загрузка...</div>)
-  }
-  if (store.isAuth) {
-    return (
-    <div>
-      <h1>{`Авторизован ${store.user.email}`}</h1>   
-        <button onClick={()=>getUsers()}>СПИСОК</button>   
-        <button onClick={()=>store.logout()}>ВЫХОД</button>
-        <ul>
-        {users.map(user=><li key={user.id}>{user.email} {user.roles} {user.activated}</li>)}
-        </ul>
-    </div>
-    )
-  } else  return ( <LoginPage /> )
+  },[store]) 
+   
+  return (
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>  
+  )  
 }
 
 export default observer(App)
