60484fb853b5038f450c9959a54a9e8bf68b5f42
SergePauli <SergePauli@yandex.ru>
2021-07-27 18:52:31 +0800

    feature:(add root css in App), stable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter  from './components/AppRouter'
+import './styles/root.css'
+
 
 const App: FC =() => {   
   return (
