5b5ac76f4e97f243167a84378f263063b2bd6a96
SergePauli <SergePauli@yandex.ru>
2021-07-27 18:56:41 +0800

    feature:(add div struture and classes in LoginPage), stable

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,9 @@ import { observer } from 'mobx-react-lite'
 import React, { useContext } from 'react'
 import { useState } from 'react'
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 import { Context } from '..'
+import '../styles/login.css'
 
 
 const LoginPage: FC = () => {
@@ -11,20 +13,39 @@ const LoginPage: FC = () => {
   const {userStore} = useContext(Context)
   return (
   <>
-    <h1>Login Form</h1>
-    <div>
-      <input onChange={e => setEmail(e.target.value)}
-        value={email}
-        type="text"
-        placeholder="email"
-      />
-      <input onChange={e => setPassword(e.target.value)}
-        value={password}
-        type="password"
-        placeholder="password"
-      />
-      <button onClick={()=>userStore.login(email,password)}>login</button>    
-      
+    <div className="login-body">
+      <div className="login-wrapper">
+        <div className="login-panel">
+          <img src="" className="logo" alt="diamond-layout"></img>
+          <div className="login-form">
+            <h2>Вход</h2>
+            <p>Нет учетной записи? <Link to="/registration">Подать заявку</Link></p>
+              <input onChange={e => setEmail(e.target.value)}
+                className="p-inputtext p-component"
+                value={email}
+                type="text"
+                placeholder="email"
+              />
+              <input onChange={e => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="password"
+              />
+              <button className='p-button p-component' onClick={()=>userStore.login(email,password)}>ВОЙТИ</button>    
+            </div>
+          </div>
+          <div className="login-image">
+            <div className="login-image-content">
+              <h1>Медиинское</h1>
+              <h1>свидетельство</h1>
+              <h1>о смерти</h1>
+              <h3>версия 3.0</h3>
+            </div>
+            <div className="login-footer">
+
+            </div>
+          </div> 
+      </div>
     </div>
   </>
 )}
