8e7a4bc5f3096f46cdef6da12ccb66066d335d71
SergePauli <SergePauli@yandex.ru>
2021-07-27 13:35:40 +0800

    feature:(create NoMatchPage to pages), stable

---
+++ src/pages/NoMatchPage.tsx
@@ -0,0 +1,13 @@
+
+import { FC } from 'react'
+
+export const NoMatchPage: FC = () => {  
+  
+
+  return (
+    <>
+      <h1>404-Page React TS FC Component</h1>
+      <div>страница не найдена</div>
+    </>
+  )
+}
