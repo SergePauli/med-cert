0f3ada2ebd668914e33346b13dcb05e63d78219e
SergePauli <SergePauli@yandex.ru>
2021-07-27 13:23:11 +0800

    refactor:(change MainPage in pages), nonstable

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
 import { FC } from 'react';
+import { Context } from '..';
 
 type MainPageProps = {}
 
-export const MainPage: FC<MainPageProps> = (props: MainPageProps) => {
+const MainPage: FC<MainPageProps> = (props: MainPageProps) => {
   const [state, setState] = useState();
-
+  const {userStore} = useContext(Context)
   useEffect(() => {}, []);
 
   return (
     <>
       <h1>MainPage React TS FC Component</h1>
-      <div>List</div>
+      <button onClick={()=>userStore.logout()}>Выход</button>
     </>
-  );
-};
+  )
+}
+export default observer(MainPage)
