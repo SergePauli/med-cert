b0f073dc9fe399db6bf2050f7a5c9baa50255b6b
SergePauli <SergePauli@yandex.ru>
2021-07-29 11:05:42 +0800

    refactor:(create static template LoginImageDiv), stable

---
+++ src/static/LoginImageDiv.js
@@ -0,0 +1,18 @@
+import React from "react"
+const LoginImageDiv = () => {
+  return (
+    <div className='login-image'>
+      <div className='login-image-content'>
+        <h1>Медиинское</h1>
+        <h1>свидетельство</h1>
+        <h1>о смерти</h1>
+        <h3>версия 3.0</h3>
+        <h3>(с учетом требований CDA_R2 уровня 3)</h3>
+      </div>
+      <div className='login-footer'>
+        <p>2021 АО "АМИАЦ"</p>
+      </div>
+    </div>
+  )
+}
+export default LoginImageDiv
