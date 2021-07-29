db7c58970f5130f1893e35bdff0467c34c49442d
SergePauli <SergePauli@yandex.ru>
2021-07-29 11:02:42 +0800

    feature:(add primeicons and praimeflex css import to App), stable

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, {FC} from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter  from './components/AppRouter'
 import './styles/root.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
 
 const App: FC =() => {   
