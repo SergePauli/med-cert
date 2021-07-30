35ef46fb8fc5c195dfc09a07ac4970c363084c6f
SergePauli <SergePauli@yandex.ru>
2021-07-30 10:55:13 +0800

    refactor:(add primereact.css to styles), stable

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter  from './components/AppRouter'
 import './styles/root.css'
+import './styles/primereact.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
