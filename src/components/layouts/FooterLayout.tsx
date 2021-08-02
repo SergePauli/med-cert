1a44c5c2b4ead82c77bbd02d09e55eebcb8d2754
SergePauli <SergePauli@yandex.ru>
2021-08-02 18:51:27 +0800

    feature:(create FooterLayout in Layouts), stable

---
+++ src/components/layouts/FooterLayout.tsx
@@ -0,0 +1,12 @@
+const FooterLayout = () => {
+  return (
+    <div className="layout-footer">
+      <div className="footer-logo-container">
+        <span className="app-name">Медицинское свидетельство о смерти</span>
+        <span className="app-profile">профиль: АДМИНИСТРАТОР</span>
+      </div>
+      <span className="copyright">2021 © ГБУЗ АО "АМИАЦ"</span>
+    </div>       
+  )
+}
+export default FooterLayout
