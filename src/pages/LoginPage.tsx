950531c078b1119270c771801f3304bed4609e6a
SergePauli <SergePauli@yandex.ru>
2021-08-02 15:08:00 +0800

    refactor:(loginPage), stable

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite'
-import { useContext, useEffect} from 'react'
+import { useContext} from 'react'
 import { useState } from 'react'
 import { FC } from 'react'
 import { Link} from 'react-router-dom'
