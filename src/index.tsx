b64b9484a34327e62b16598f7e2684c091a12b3f
SergePauli <SergePauli@yandex.ru>
2021-08-03 16:31:26 +0800

    feature:(add layoutStore to Context in index.tsx), stable

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React, { createContext } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import LayoutStore from './store/layoutStore'
 import UserStore from './store/userStore'
 interface IState{
   userStore: UserStore
+  layoutStore: LayoutStore
 }
 const userStore = new UserStore()
-export const Context = createContext<IState>({userStore}) 
+const layoutStore = new LayoutStore()
+export const Context = createContext<IState>({userStore, layoutStore}) 
 ReactDOM.render(  
-  <Context.Provider value={{userStore}} >
+  <Context.Provider value={{userStore, layoutStore}} >
     <App />  
   </Context.Provider> , 
   document.getElementById('root')
