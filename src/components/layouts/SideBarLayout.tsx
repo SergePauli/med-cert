6a6530e61ccc633c586cb5eea45688c870c1f99d
SergePauli <SergePauli@yandex.ru>
2021-08-06 09:39:45 +0800

    fix:(SideBarLayout ), nonstable

--- src/components/layouts/SideBarLayout.tsx
+++ src/components/layouts/SideBarLayout.tsx
@@ -1,99 +1,46 @@
-import avatar from "../images/logo.svg"
+import avatar from "../../images/logo.svg"
 import { useContext } from "react"
 import { observer } from "mobx-react-lite"
 import { Context } from "../.."
-import { Sidebar } from 'primereact/sidebar'
-import { PanelMenu } from 'primereact/panelmenu'
 import { Link } from "react-router-dom"
-import { LayoutMenu } from "../customUI/LayoutMenu"
-const SideBarLayout = observer(() => {
-   const {layoutStore} = useContext(Context)
-   const topMenuItems = [
-      { label: "Избранное",      
-        items: [{label:"Главная", icon:"pi pi-fw pi-home", url:"/"}]     
-      }      
+import { CERTIFICATE_ROUTE, HOME_ROUTE, LIST_ROUTE } from "../../utils/consts"
+import { SideBarMenu } from "../menus/SideBarMenu"
+import { MenuItem } from "primereact/menuitem"
+type SideBarLayoutProps = {activeUrl: string}
+const SideBarLayout = observer((props: SideBarLayoutProps) => {
+   const {userStore} = useContext(Context)
+   const items:MenuItem[] = [    
+      { label: "Свидетельство", className: "layout-root-menuitem",     
+        items:[{label:"Форма", icon:"pi-file", items:[{label:"Ввод", icon:"pi-pencil", url:CERTIFICATE_ROUTE}, {label:"Заменить", icon:"pi-refresh", url:"/#"},{label:"Удалить", icon:"pi-trash", url:"/#"}, {label:"Сохранить", icon:"pi-save", disabled:true, url:"/#"},{label:"Отменить", icon:"pi-times", disabled:true, url:"/#"}]},
+        { label: "Списки",  icon:"pi-list", items: [{label:"Свидетельства", icon:"pi-id-card", url:LIST_ROUTE},{label:"Журнал", icon:"pi-align-left", url:"/#"}]},]}, 
+      {separator: true},
+      { label: "Сеанс", className: "layout-root-menuitem",      
+        items: [{label:"Главная", icon:"pi-home", url:HOME_ROUTE},{label:"Выход", icon:"pi-sign-out", command:()=>{userStore.logout()}}]     
+      },     
    ]   
+  const url = props.activeUrl
+  const findActive = (items: MenuItem[])=>{
+    let rez:MenuItem = {} 
+    items.forEach((item)=>{        
+     if (item.url == url) rez = item
+       if (item.items){
+        let active = findActive(item.items)
+        if (active.label) rez = active
+       } 
+      })      
+      return rez
+    }
+  const active =  findActive(items) 
+  
+  if (active.label) active.className='active-menuitem'
   return (
-    <Sidebar visible={layoutStore.sideBarVisible()} className='layout-sidebar' position="left" onHide={()=>layoutStore.setSideBarVisible(false)}>      
+    <div className='layout-sidebar'>      
         <Link className='logo' to='#/'>
           <img id='app-logo' className='logo-image' src={avatar} alt='diamond layout' />
           <span className='app-name'>МЕДСС</span>
-        </Link>
-        <div className='layout-menu-container'>
-          <LayoutMenu model={topMenuItems} className='layout-menu'/>
-          <PanelMenu className='layout-menu'/>
-          <LayoutMenu className='layout-menu'/>
-
-          {/* <ul className='layout-menu' role='menu'>
-            <li className='layout-root-menuitem' role='menuitem'>
-              <button type='button' className='p-ripple p-link'>
-                <i className='layout-menuitem-icon pi pi-fw pi-home'></i>
-                <span className='layout-menuitem-text'>Главная</span>
-                <i className='pi pi-fw pi-angle-down layout-submenu-toggler'></i>
-              </button>
-              <div className='layout-root-menuitem'>
-                <div className='layout-menuitem-root-text' style={{ textTransform: "uppercase" }}>
-                  Избранное
-                </div>
-              </div>
-              <ul className='layout-menu' role='menu'>
-                <li role='menuitem'>
-                  <a className='p-ripple' href='/'>
-                    <i className='layout-menuitem-icon pi pi-fw pi-home'></i>
-                    <span className='layout-menuitem-text'>Главная</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className='layout-root-menuitem' role='menuitem'>
-              <button type='button' className='p-ripple p-link'>
-                <i className='layout-menuitem-icon pi pi-fw pi-id-card'></i>
-                <span className='layout-menuitem-text'>Операции</span>
-                <i className='pi pi-fw pi-angle-down layout-submenu-toggler'></i>
-              </button>
-              <div className='layout-root-menuitem'>
-                <div className='layout-menuitem-root-text' style={{ textTransform: "uppercase" }}>
-                  Операции
-                </div>
-              </div>
-              <ul className='layout-menu' role='menu'>
-                <li className='' role='menuitem'>
-                  <a className='p-ripple active-route' href='/list' aria-current='page'>
-                    <i className='layout-menuitem-icon pi pi-fw pi-list'></i>
-                    <span className='layout-menuitem-text'>Список</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className='layout-root-menuitem' role='menuitem'>
-              <button type='button' className='p-ripple p-link'>
-                <i className='layout-menuitem-icon pi pi-fw pi-id-card'></i>
-                <span className='layout-menuitem-text'>Ввод</span>
-                <i className='pi pi-fw pi-angle-down layout-submenu-toggler'></i>
-              </button>
-              <div className='layout-root-menuitem'>
-                <div className='layout-menuitem-root-text' style={{ textTransform: "uppercase" }}>
-                  Ввод
-                </div>
-              </div>
-              <ul className='layout-menu' role='menu'>
-                <li className='' role='menuitem'>
-                  <a className='p-ripple active-route' href='#/' aria-current='page'>
-                    <i className='layout-menuitem-icon pi pi-fw pi-check-square'></i>
-                    <span className='layout-menuitem-text'>пункты 5,6</span>
-                  </a>
-                </li>
-                <li className='' role='menuitem'>
-                  <a className='p-ripple active-route' href='#/' aria-current='page'>
-                    <i className='layout-menuitem-icon pi pi-fw pi-check-square'></i>
-                    <span className='layout-menuitem-text'>пункты 7,8</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul> */}
-        </div>
-      </Sidebar>      
+        </Link>       
+        <SideBarMenu model={items} />  
+    </div>      
   )
 })
 export default SideBarLayout
