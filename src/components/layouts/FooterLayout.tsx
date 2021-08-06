b57ed660550866971ac8107f0e28c7590aa2f3cd
SergePauli <SergePauli@yandex.ru>
2021-08-06 09:37:22 +0800

    feature:(add role info in FooterLayout ),stable

--- src/components/layouts/FooterLayout.tsx
+++ src/components/layouts/FooterLayout.tsx
@@ -2,8 +2,8 @@ const FooterLayout = () => {
   return (
     <div className="layout-footer">
       <div className="footer-logo-container">
-        <span className="app-name">Медицинское свидетельство о смерти</span>
-        <span className="app-profile">профиль: АДМИНИСТРАТОР</span>
+        <span className="app-name">Медицинское свидетельство о смерти:</span>
+        <span className="app-profile">АДМИНИСТРАТОР</span>
       </div>
       <span className="copyright">2021 © ГБУЗ АО "АМИАЦ"</span>
     </div>       
