41ec37a1f72dc46307a6319ac79a862ccc04762b
SergePauli <SergePauli@yandex.ru>
2021-08-06 09:41:39 +0800

    feature:(use MainLayot in MainPage), stable

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,6 +2,8 @@ import { observer } from 'mobx-react-lite';
 import React, { useContext, useEffect, useState } from 'react';
 import { FC } from 'react';
 import { Context } from '..';
+import { MainLayout, MainLayoutProps } from '../components/layouts/MainLayout';
+import { HOME_ROUTE } from '../utils/consts';
 
 type MainPageProps = {}
 
@@ -9,11 +11,20 @@ const MainPage: FC<MainPageProps> = (props: MainPageProps) => {
   const [state, setState] = useState();
   const {userStore} = useContext(Context)
   useEffect(() => {}, []);
-
+  const layoutParams: MainLayoutProps = {
+    isLayoutStaticInactive: false, 
+    isTabletOrMobile: false, 
+    url: HOME_ROUTE,
+    content:( 
+      <>     
+        <h1>MainPage React TS FC Component</h1>
+        <button onClick={()=>userStore.logout()}>Выход</button>
+      </>
+    )    
+  }
   return (
     <>
-      <h1>MainPage React TS FC Component</h1>
-      <button onClick={()=>userStore.logout()}>Выход</button>
+      <MainLayout {...layoutParams} />
     </>
   )
 }
