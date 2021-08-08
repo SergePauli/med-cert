f12cf91af752b2add2c787b5d7f3d55320da46e2
SergePauli <SergePauli@yandex.ru>
2021-08-08 15:10:44 +0800

    refactor:(MainPage in pages), stable

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,23 +2,22 @@ import { observer } from 'mobx-react-lite';
 import React, { useContext, useEffect, useState } from 'react';
 import { FC } from 'react';
 import { Context } from '..';
-import { MainLayout, MainLayoutProps } from '../components/layouts/MainLayout';
+import MainLayout from '../components/layouts/MainLayout';
 import { HOME_ROUTE } from '../utils/consts';
 
 type MainPageProps = {}
 
 const MainPage: FC<MainPageProps> = (props: MainPageProps) => {
   const [state, setState] = useState();
-  const {userStore} = useContext(Context)
+  const {userStore, layoutStore} = useContext(Context)
   useEffect(() => {}, []);
-  const layoutParams: MainLayoutProps = {
-    isLayoutStaticInactive: false, 
-    isTabletOrMobile: false, 
+  const layoutParams= {
+    title: 'Главная',     
     url: HOME_ROUTE,
     content:( 
       <>     
         <h1>MainPage React TS FC Component</h1>
-        <button onClick={()=>userStore.logout()}>Выход</button>
+        {`ml_lsi${layoutStore.layoutStaticInactive()}_tom${layoutStore.tabletOrMobile()}`}
       </>
     )    
   }
