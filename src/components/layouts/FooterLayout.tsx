8f24b6df56d8dba6c53beae4ef76eeadf38f81fc
SergePauli <SergePauli@yandex.ru>
2021-08-08 15:13:09 +0800

    feature:(improve app-profile in FooterLayout), stable

--- src/components/layouts/FooterLayout.tsx
+++ src/components/layouts/FooterLayout.tsx
@@ -2,7 +2,7 @@ const FooterLayout = () => {
   return (
     <div className="layout-footer">
       <div className="footer-logo-container">
-        <span className="app-name">Медицинское свидетельство о смерти:</span>
+        <span className="app-name">ГАУЗ АО ГП №1:</span>
         <span className="app-profile">АДМИНИСТРАТОР</span>
       </div>
       <span className="copyright">2021 © ГБУЗ АО "АМИАЦ"</span>
