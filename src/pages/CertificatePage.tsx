b72b171ec9c66f2b1a9d9c1a8aae55089b88630c
SergePauli <SergePauli@yandex.ru>
2021-08-17 11:48:41 +0800

    refactor:(add section support to CertificatePage in pages), stable

--- src/pages/CertificatePage.tsx
+++ src/pages/CertificatePage.tsx
@@ -1,27 +1,42 @@
 import { Card } from 'primereact/card'
 import { Column } from 'primereact/column'
 import { DataTable } from 'primereact/datatable'
-import { Dropdown } from 'primereact/dropdown'
-import { InputText } from 'primereact/inputtext'
-import { Calendar } from 'primereact/calendar'
+
 import '../styles/components/Calendar.css'
 import '../styles/components/Divider.css'
 import '../styles/components/Button.css'
 import '../styles/pages/CertificatePage.css'
-import { useEffect, useState } from 'react'
-import { FC } from 'react'
+
+import { useContext, FC } from 'react'
 import MainLayout from '../components/layouts/MainLayout'
-import { CERTIFICATE_ROUTE, CERT_KIND } from '../utils/consts'
+import { CERTIFICATE_ROUTE } from '../utils/consts'
 import { Divider } from 'primereact/divider'
 import { Button } from 'primereact/button'
 import { Avatar } from 'primereact/avatar'
+import { IRouteMatch } from '../models/IRouteMatch'
+import { IRouteProps } from '../models/IRouteProps'
+import Section0 from '../components/c_sections/section_0'
+import Section1 from '../components/c_sections/section_1'
+import { Context } from '..'
 
-type CertificatePageProps = {}
+interface IMatch extends IRouteMatch {  
+  params: {id: number}
+}
+interface CertificatePageProps extends IRouteProps { 
+    match: IMatch  
+}
 
-export const CertificatePage: FC<CertificatePageProps> = (props: CertificatePageProps) => {
-  const [state, setState] = useState()
-
-  useEffect(() => {}, [])
+export const CertificatePage: FC<CertificatePageProps> = (props: CertificatePageProps) => {  
+  const { certificateStore, userStore } = useContext(Context)   
+   const cert_number = certificateStore.series()
+   console.log(cert_number, userStore.isAuth())
+  const secton_router = ()=>{
+    switch (props.location.search) {
+      case "?q=0": return <Section0 />
+      case "?q=1": return <Section1 />
+      default: return <Section0 /> 
+    } 
+  }
   const suggestions = [
     {code:"У3-3", suggestion:"Необходимо выбрать тип свидетельства", done:true }
   ] 
@@ -40,45 +55,10 @@ export const CertificatePage: FC<CertificatePageProps> = (props: CertificatePage
   }
   const layoutParams = {
     title: 'Медицинское свидетельство о смерти',     
-    url: CERTIFICATE_ROUTE,
-    content:(
-      <>
+    url: `${CERTIFICATE_ROUTE}/${props.match.params.id}${props.location.search}`,
+    content:(<>
       <div className="p-d-flex p-jc-center">
-        <Card className="p-mr-2 p-mb-2" style={{maxWidth:'500px'}}>
-            <h5>Сведения о документе</h5>
-            <div className="p-fluid p-formgrid p-grid">
-                <div className="p-field p-col-12 p-md-6">
-                  <label htmlFor="firstname6">Серия</label>
-                  <InputText id="firstname6" disabled type="text" placeholder='Заполняется автоматически' />
-                </div>
-                <div className="p-field p-col-12 p-md-6">
-                  <label htmlFor="lastname6">Номер</label>
-                  <InputText id="lastname6" disabled type="text" placeholder='Заполняется автоматически'/>
-                </div>              
-                
-                <div className="p-field p-col-12 p-md-6">
-                  <label htmlFor="state">Вид свидетельства</label>
-                  <Dropdown inputId="state"  placeholder="Выбрать" autoFocus 
-                  options={CERT_KIND.filter((item)=>"1 2".includes(item.code))} optionLabel="name"/>
-                </div>
-                <div className="p-field p-col-12 p-md-6">                    
-                  <label htmlFor="icon">Дата</label>
-                  <Calendar id="icon"  dateFormat="dd/mm/yy" value={new Date()} disabled showIcon />
-                </div>
-            </div>
-            <Divider/>
-            <div className="p-grid p-mt-2">              
-                <div className="p-col-6 p-d-flex p-flex-column p-jc-left p-ai-center p-flex-md-row p-flex-wrap">
-                  <div className="overview-number p-mr-1 p-mb-1" style={{padding:'0.1rem'}}>Статус:</div>
-                  <div className="overview-subtext p-mr-1 p-mb-1"style={{fontWeight:600,background: '#c8e6c9', color: '#256029', padding:'0.2rem'}}>АКТУАЛЬНОЕ</div>
-                </div>
-                <div className="p-col-6 p-d-flex p-flex-column p-jc-left p-ai-center p-flex-md-row p-flex-wrap"
-                  style={{borderLeft:'1px #dee2e6', borderLeftStyle:'solid'}}>
-                  <div className="overview-number p-mr-1 p-mb-1" style={{padding:'0.1rem'}}>Ранее выданные:</div>
-                  <div className="overview-subtext p-mr-1 p-mb-1" style={{fontWeight:600,padding:'0.2rem'}}>НЕТ</div>
-                </div>              
-            </div>
-        </Card>
+        {secton_router()}
         <Card className="p-mr-2 p-mb-2 p-suggestion" header={suggestionHeader}>            
             <DataTable className="p-datatable-sm" rowClassName={rowClass} value={suggestions}>
               <Column field="code" header="Код"></Column>
@@ -94,8 +74,7 @@ export const CertificatePage: FC<CertificatePageProps> = (props: CertificatePage
         <Button icon="pi pi-save" className="p-button-secondary p-button-action p-mr-4 p-mb-2 p-shadow-3" label="Сохранить" disabled />
         <Button icon="pi pi-angle-right" iconPos="right" className="p-button-action p-mr-4 p-mb-2 p-shadow-3"  label="Далее"  />  
       </div>
-      </>
-    )
+    </>)
   }  
   return (
     <>
