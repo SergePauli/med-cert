51894d6bba892b985131639f3d70addf6a5cdf21
SergePauli <SergePauli@yandex.ru>
2021-08-17 11:50:40 +0800

    feature: (add Certificate ID and section navigation support in routes and MainPage ), stable

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -12,7 +12,9 @@ import { Context } from '..'
 type MainPageProps = {}
 
 const MainPage: FC<MainPageProps> = (props: MainPageProps) => {
-  const {userStore} = useContext(Context)
+  const {userStore, certificateStore} = useContext(Context)
+  const cert_id = certificateStore.id()
+  certificateStore.setSeries('test')
   const news =[{version:"3.00",record:"с учетом требований CDA_R2 уровня 3"},
   {version:"2.10",record:"Добавлена возможность 'грязной' выборки в ОТЧЕТНОМ БЛОКЕ"},
   {version:"2.09",record:"Добавлена возможность ввода периода времени в п.19II"}]
@@ -22,7 +24,8 @@ const MainPage: FC<MainPageProps> = (props: MainPageProps) => {
     content:( 
       <>     
         <div className="p-d-flex p-flex-column p-jc-around p-flex-md-row p-flex-wrap">
-          <Button style={{minWidth:'243px'}} className="p-mr-2 p-mb-2 p-shadow-3" label="Ввод свидетельства"  title="Форма ввода свидетельства" onClick={(e)=>{userStore.history().push(CERTIFICATE_ROUTE)}}/>
+          <Button style={{minWidth:'243px'}} className="p-mr-2 p-mb-2 p-shadow-3" label="Ввод свидетельства"  title="Форма ввода свидетельства" 
+          onClick={(e)=>{userStore.history().push(`${CERTIFICATE_ROUTE}/${cert_id}?q=0`)}}/>
             <Button style={{minWidth:'243px'}} className="p-button-secondary p-mr-2 p-mb-2 p-shadow-3" label="Настройки" title="Настройки учетной записи пользователя"/>
             <Button style={{minWidth:'243px'}} className="p-button-secondary p-mr-2 p-mb-2 p-shadow-3" id="reports" label="Отчеты" title="Формирование отчетов" />
             <Button style={{minWidth:'243px'}} className="p-mr-2 p-mb-2 p-shadow-3" label="Перинатальное свидетельство" /> 
