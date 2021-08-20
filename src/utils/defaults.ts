e73dc734125fcb5c16445266e424c02b75085fc5
SergePauli <SergePauli@yandex.ru>
2021-08-20 15:51:47 +0800

    feature:(create Defaults arrays in utils), stable

---
+++ src/utils/defaults.ts
@@ -0,0 +1,24 @@
+export const DEFAULT_CERT_SUGGESTIONS = [
+  { section: "0", code: "0", suggestion: "Необходимо выбрать тип свидетельства", done: false },
+  { section: "1", code: "п.2", suggestion: "Указание пола умершего является обязательным для заполнения", done: false },
+]
+export const NULL_FLAVORS = [
+  { code: "NI", name: "Нет информации" },
+  { code: "INV", name: "Недопустимое значение" },
+  { code: "DER", name: "Извлекаемое значение" },
+  { code: "OTH", name: "Другое" },
+  { code: "NINF", name: "Минус бесконечность" },
+  { code: "PINF", name: "Плюс бесконечность" },
+  { code: "UNC", name: "Кодирование не проводилось" },
+  { code: "MSK", name: "Скрыто" },
+  { code: "NA", name: "Неприменимо" },
+  { code: "UNK", name: "Неизвестно" },
+  { code: "ASKU", name: "Запрошено, но неизвестно" },
+  { code: "NAV", name: "Временно недоступно" },
+  { code: "NASK", name: "Не запрашивалось" },
+  { code: "QS", name: "Достаточное количество" },
+  { code: "TRC", name: "Трудноразличимо" },
+]
+export const ASKU = 10
+export const NA = 8
+export const UNK = 9
