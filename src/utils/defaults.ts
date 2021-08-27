1bb0a894cd6f1c46512c8349331b6896f01ebd4f
SergePauli <SergePauli@yandex.ru>
2021-08-27 11:53:50 +0800

    feature:(add suggestions of section1 in defaults), stable

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,7 +1,31 @@
 export const DEFAULT_CERT_SUGGESTIONS = [
   { section: "0", code: "0", suggestion: "Необходимо выбрать тип свидетельства", done: false },
+  {
+    section: "1",
+    code: "п.1",
+    suggestion: "Поля ФИО должны быть заполнены, если умерший идентифицирован",
+    done: false,
+  },
   { section: "1", code: "п.2", suggestion: "Указание пола умершего является обязательным для заполнения", done: false },
+  {
+    section: "1",
+    code: "п.3",
+    suggestion: "Указание даты рождения или причины ее отсутствия - обязательно для заполнения",
+    done: false,
+  },
+  {
+    section: "1",
+    code: "п.4",
+    suggestion: "Указание даты смерти или причины ее отсутствия - обязательно для заполнения",
+    done: false,
+  },
 ]
+export const CERT_TYPE_SUG = 0
+export const PERSON_NAME_SUG = 1
+export const PATIENT_GENDER_SUG = 2
+export const PATIENT_BIRTHDAY_SUG = 3
+export const CERT_DEATH_THIME_SUG = 4
+
 export const NULL_FLAVORS = [
   { code: "NI", name: "Нет информации" },
   { code: "INV", name: "Недопустимое значение" },
@@ -22,3 +46,7 @@ export const NULL_FLAVORS = [
 export const ASKU = 10
 export const NA = 8
 export const UNK = 9
+
+export const MALE = 1
+export const FEMALE = 2
+export const NOGENDER = 4
