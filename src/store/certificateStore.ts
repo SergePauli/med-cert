94b0a9b58e4598e5193a35cfd1bfbd61d5420a45
SergePauli <SergePauli@yandex.ru>
2021-10-19 09:51:58 +0800

    feature:(add reactions to CertificateStore), nonstable

--- src/store/certificateStore.ts
+++ src/store/certificateStore.ts
@@ -1,39 +1,58 @@
-import { makeAutoObservable } from "mobx"
+import { autorun, configure, makeAutoObservable } from "mobx"
 import Certificate from "../models/FormsData/Certificate"
 import { INullFlavor } from "../models/INullFlavor"
 import { IPatient } from "../models/IPatient"
-import { IReference } from "../models/IReference"
+
 import { ISuggestions } from "../models/ISuggestions"
 import { ICertificateResponse } from "../models/responses/ICertificateResponse"
+import { DISEASE_DEADTH_KIND } from "../NSI/1.2.643.5.1.13.13.99.2.21"
 import {
+  BASIS_DERMINING_SUG,
   CERT_DEATH_THIME_SUG,
   CERT_TYPE_SUG,
+  CHILD_WEIGHT_SUG,
   DEATH_AREA_SUG,
+  DEATH_KINDS_SUG,
   DEATH_PLACE_SUG,
+  DEATH_PLACE_TYPE_SUG,
   DEFAULT_CERT_SUGGESTIONS,
+  EDUCATION_LEVEL_SUG,
+  EST_MEDIC_SUG,
   IDNUMBER_SUG,
   IDSERIES_SUG,
   ID_CARD_TYPES,
   IORGCODE_SUG,
   IORGDATE_SUG,
   IORGNAME_SUG,
+  KIND_DEATH_REASON_SUG,
   LIFE_AREA_SUG,
   LIFE_PLACE_SUG,
+  MARITAL_STATUS_SUG,
+  MOTHER_ADDRESS_SUG,
+  MOTHER_BIRTHDAY_SUG,
+  MOTHER_FIO_SUG,
   NA,
+  NUMBER_PREGNANCY_SUG,
   OMS_SUG,
   PASSPORT_RF,
   PATIENT_BIRTHDAY_SUG,
   PATIENT_GENDER_SUG,
   PERSON_NAME_SUG,
   SNILS_SUG,
+  SOCIAL_STATUS_SUG,
+  TERMS_PREGNANCY_SUG,
 } from "../utils/defaults"
-
+configure({
+  enforceActions: "never",
+})
 export default class CertificateStore {
   private _cert: Certificate
   private _suggestions: ISuggestions[]
   private _identified: boolean
   private _fromRelatives: boolean
+  disposers: (() => void)[]
   constructor() {
+    this.disposers = []
     this._cert = new Certificate({
       patient: {
         identity: {
@@ -46,14 +65,195 @@ export default class CertificateStore {
     this._fromRelatives = this._identified && this._cert.patient.identity === undefined
     this._suggestions = DEFAULT_CERT_SUGGESTIONS
     makeAutoObservable(this)
+    this.disposers[0] = autorun(() => {
+      const fio = this._cert.patient?.person.fio
+      const isPersonNameSug =
+        fio !== undefined &&
+        (fio.family.trim().length === 0 ||
+          fio.given_1.trim().length === 0 ||
+          (fio.given_2 !== undefined && fio.given_2.trim().length === 0))
+      this._suggestions[PERSON_NAME_SUG].done = !isPersonNameSug
+    })
+    this.disposers[1] = autorun(() => {
+      this._suggestions[CERT_TYPE_SUG].done = this.cert.certType !== undefined
+    })
+    this.disposers[2] = autorun(() => {
+      this._suggestions[PATIENT_GENDER_SUG].done = this.cert.patient.gender !== undefined
+    })
+    this.disposers[3] = autorun(() => {
+      const patient = this._cert.patient
+      const isBirthDay =
+        patient.birth_date === undefined &&
+        patient.nullFlavors().findIndex((element) => element.parent_attr === "birth_date") === -1
+      this._suggestions[PATIENT_BIRTHDAY_SUG].done = !isBirthDay
+    })
+    this.disposers[4] = autorun(() => {
+      const cert = this._cert
+      const isDeathDay =
+        cert.deathDatetime === undefined &&
+        cert.nullFlavors.findIndex((element) => element.parent_attr === "death_datetime") === -1
+      this._suggestions[CERT_DEATH_THIME_SUG].done = !isDeathDay
+    })
+    this.disposers[5] = autorun(() => {
+      const person = this._cert.patient.person
+      const isSNILS =
+        (person.SNILS === undefined || person.SNILS.length < 14) &&
+        person.nullFlavors().findIndex((element) => element.parent_attr === "SNILS") === -1
+      this._suggestions[SNILS_SUG].done = !isSNILS
+    })
+    this.disposers[6] = autorun(() => {
+      const oms = this._cert.policyOMS
+      const isOMS =
+        (oms === undefined || oms.length < 7) &&
+        this._cert.nullFlavors.findIndex((element) => element.parent_attr === "policy_OMS") === -1
+      this._suggestions[OMS_SUG].done = !isOMS
+    })
+    this.disposers[7] = autorun(() => {
+      const code = this._cert.patient.identity?.issueOrgCode
+      const isCODE =
+        this._cert.patient.identity === undefined ||
+        ((code === undefined || code.length < 1) &&
+          this._cert.patient.identity.nullFlavors().findIndex((element) => element.parent_attr === "issueOrgCode") ===
+            -1)
+      this._suggestions[IORGCODE_SUG].done = !isCODE
+    })
+    this.disposers[8] = autorun(() => {
+      const patient = this._cert.patient
+      const isIORGDate =
+        (patient.identity === undefined &&
+          patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1) ||
+        patient.identity?.issueOrgDate === undefined
+      this._suggestions[IORGDATE_SUG].done = !isIORGDate
+    })
+    this.disposers[9] = autorun(() => {
+      const patient = this._cert.patient
+      const isIORGName =
+        (patient.identity?.issueOrgName === undefined || patient.identity?.issueOrgName.length < 15) &&
+        patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1
+      this._suggestions[IORGNAME_SUG].done = !isIORGName
+    })
+    this.disposers[10] = autorun(() => {
+      const patient = this._cert.patient
+      const isIDSeries =
+        (patient.identity?.series === undefined || patient.identity?.series.length < 1) &&
+        patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1
+      this._suggestions[IDSERIES_SUG].done = !isIDSeries
+    })
+    this.disposers[11] = autorun(() => {
+      const patient = this._cert.patient
+      const isIDNumber =
+        (patient.identity?.number === undefined || patient.identity?.number.length < 1) &&
+        patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1
+      this._suggestions[IDNUMBER_SUG].done = !isIDNumber
+    })
+    this.disposers[12] = autorun(() => {
+      const isLifeArea =
+        (this._cert.patient.address === undefined ||
+          this._cert.patient.address.aoGUID === undefined ||
+          this._cert.patient.address.postalCode === undefined ||
+          this._cert.patient.address.housenum === undefined) &&
+        this._cert.patient.nullFlavors().findIndex((element) => element.parent_attr === "addr") === -1
+      this._suggestions[LIFE_PLACE_SUG].done = !isLifeArea
+    })
+    this.disposers[13] = autorun(() => {
+      const isDeathArea =
+        (this._cert.deathAddr === undefined ||
+          this._cert.deathAddr.aoGUID === undefined ||
+          this._cert.deathAddr.postalCode === undefined ||
+          this._cert.deathAddr.housenum === undefined) &&
+        this._cert.nullFlavors.findIndex((element) => element.parent_attr === "death_addr") === -1
+      this._suggestions[DEATH_PLACE_SUG].done = !isDeathArea
+    })
+    this.disposers[14] = autorun(() => {
+      const isDeathAreaType =
+        this._cert.deathAreaType === undefined &&
+        this._cert.nullFlavors.findIndex((element) => element.parent_attr === "death_area_type") === -1
+      this._suggestions[DEATH_AREA_SUG].done = !isDeathAreaType
+    })
+    this.disposers[15] = autorun(() => {
+      const isLifeAreaType =
+        this._cert.lifeAreaType === undefined &&
+        this._cert.nullFlavors.findIndex((element) => element.parent_attr === "life_area_type") === -1
+      this._suggestions[LIFE_AREA_SUG].done = !isLifeAreaType
+    })
+    this.disposers[16] = autorun(() => {
+      this._suggestions[DEATH_PLACE_TYPE_SUG].done = this._cert.deathPlace !== undefined
+    })
+    this.disposers[17] = autorun(() => {
+      const childInfo = this._cert.childInfo
+      const isTERMS_PREGNANCY =
+        childInfo === undefined ||
+        childInfo.termPregnancy !== undefined ||
+        childInfo.nullFlavors.findIndex((element) => element.parent_attr === "term_pregnancy") !== -1
+      this._suggestions[TERMS_PREGNANCY_SUG].done = isTERMS_PREGNANCY
+    })
+    this.disposers[18] = autorun(() => {
+      const childInfo = this._cert.childInfo
+      const isCHILD_WEIGHT = childInfo === undefined || childInfo.weight !== undefined
+      this._suggestions[CHILD_WEIGHT_SUG].done = isCHILD_WEIGHT
+    })
+    this.disposers[19] = autorun(() => {
+      const childInfo = this._cert.childInfo
+      const isNUMBER_PREGNANCY = childInfo === undefined || childInfo.whichAccount !== undefined
+      this._suggestions[NUMBER_PREGNANCY_SUG].done = isNUMBER_PREGNANCY
+    })
+    this.disposers[20] = autorun(() => {
+      const childInfo = this._cert.childInfo
+      const isMOTHER_FIO =
+        childInfo === undefined ||
+        (childInfo.relatedSubject !== undefined &&
+          (childInfo.relatedSubject.nullFlavors.findIndex((item) => item.parent_attr === "person_name") !== -1 ||
+            childInfo.relatedSubject.fio !== undefined))
+      this._suggestions[MOTHER_FIO_SUG].done = isMOTHER_FIO
+    })
+    this.disposers[21] = autorun(() => {
+      const childInfo = this._cert.childInfo
+      const isMOTHER_BIRTHDAY =
+        childInfo === undefined ||
+        (childInfo.relatedSubject !== undefined &&
+          (childInfo.relatedSubject.nullFlavors.findIndex((item) => item.parent_attr === "birthTime") !== -1 ||
+            childInfo.relatedSubject.birthTime !== undefined))
+      this._suggestions[MOTHER_BIRTHDAY_SUG].done = isMOTHER_BIRTHDAY
+    })
+    this.disposers[22] = autorun(() => {
+      const childInfo = this._cert.childInfo
+      const isMOTHER_ADDRESS =
+        childInfo === undefined ||
+        (childInfo.relatedSubject !== undefined &&
+          (childInfo.relatedSubject.nullFlavors.findIndex((item) => item.parent_attr === "addr") !== -1 ||
+            (childInfo.relatedSubject.addr !== undefined &&
+              childInfo.relatedSubject.addr.aoGUID !== undefined &&
+              (childInfo.relatedSubject.addr.houseGUID !== undefined ||
+                childInfo.relatedSubject.addr.housenum !== undefined))))
+      this._suggestions[MOTHER_ADDRESS_SUG].done = isMOTHER_ADDRESS
+    })
+    this.disposers[23] = autorun(() => {
+      this._suggestions[MARITAL_STATUS_SUG].done = this._cert.maritalStatus !== undefined
+    })
+    this.disposers[24] = autorun(() => {
+      this._suggestions[EDUCATION_LEVEL_SUG].done = this._cert.educationLevel !== undefined
+    })
+    this.disposers[25] = autorun(() => {
+      this._suggestions[SOCIAL_STATUS_SUG].done = this._cert.socialStatus !== undefined
+    })
+    this.disposers[26] = autorun(() => {
+      this._suggestions[DEATH_KINDS_SUG].done = this._cert.deathKind !== undefined
+    })
+    this.disposers[27] = autorun(() => {
+      this._suggestions[KIND_DEATH_REASON_SUG].done =
+        (this._cert.deathKind !== undefined && this._cert.deathKind === DISEASE_DEADTH_KIND) ||
+        (this._cert.extReasonTime !== undefined && this._cert.extReasonDescription !== undefined)
+    })
+    this.disposers[28] = autorun(() => {
+      this._suggestions[EST_MEDIC_SUG].done = this._cert.establishedMedic !== undefined
+    })
+    this.disposers[29] = autorun(() => {
+      this._suggestions[BASIS_DERMINING_SUG].done = this._cert.basisDetermining !== undefined
+    })
   }
 
   get cert() {
     return this._cert
-  }
-  setCert_type(cert_type: IReference) {
-    this._cert.certType = cert_type
-    this.changeSuggestionsEntry(CERT_TYPE_SUG, false)
   }
   get identified() {
     return this._identified
@@ -73,7 +273,6 @@ export default class CertificateStore {
       this._cert.patient.person.nullFlavors().push({ parent_attr: "person_name", code: NA })
       this.fromRelatives = false
     }
-    this.checkFio()
   }
 
   get fromRelatives() {
@@ -85,196 +284,6 @@ export default class CertificateStore {
     if (value) this.identified = true
   }
 
-  setGender(gender: number | undefined) {
-    this._cert.patient.gender = gender
-    const isGenderSug = this._cert.patient.gender === undefined
-    this.changeSuggestionsEntry(PATIENT_GENDER_SUG, isGenderSug)
-  }
-  checkFio() {
-    const fio = this._cert.patient.person.fio
-    const isPersonNameSug =
-      fio !== undefined &&
-      (fio.family.trim().length === 0 ||
-        fio.given_1.trim().length === 0 ||
-        (fio.given_2 !== undefined && fio.given_2.trim().length === 0))
-    this.changeSuggestionsEntry(PERSON_NAME_SUG, isPersonNameSug)
-  }
-  checkBirthDay() {
-    const patient = this._cert.patient
-    const isBirthDay =
-      patient.birth_date === undefined &&
-      patient.nullFlavors().findIndex((element) => element.parent_attr === "birth_date") === -1
-    this.changeSuggestionsEntry(PATIENT_BIRTHDAY_SUG, isBirthDay)
-  }
-  setBirthDay(value: Date | undefined, isYear: boolean) {
-    const patient = this._cert.patient
-    if (value && !isYear) {
-      if (patient.birth_date === undefined)
-        patient.setNullFlavors(patient.nullFlavors().filter((element) => element.parent_attr !== "birth_date"))
-      patient.birth_date = value
-      patient.birth_year = undefined
-      this.changeSuggestionsEntry(PATIENT_BIRTHDAY_SUG, false)
-    } else if (value && isYear) {
-      if (patient.birth_date === undefined)
-        patient.setNullFlavors(patient.nullFlavors().filter((element) => element.parent_attr !== "birth_date"))
-      patient.birth_date = value
-      patient.birth_year = (patient.birth_date as Date).getFullYear()
-      this.changeSuggestionsEntry(PATIENT_BIRTHDAY_SUG, false)
-    } else {
-      this._cert.patient.birth_date = undefined
-      this._cert.patient.birth_year = undefined
-      this.checkBirthDay()
-    }
-  }
-  checkDeathDay() {
-    const cert = this._cert
-    const isDeathDay =
-      cert.deathDatetime === undefined &&
-      cert.nullFlavors().findIndex((element) => element.parent_attr === "death_datetime") === -1
-    this.changeSuggestionsEntry(CERT_DEATH_THIME_SUG, isDeathDay)
-  }
-  setDeathDay(value: Date | undefined, isYear: boolean) {
-    const cert = this._cert
-    if (value && !isYear) {
-      cert.deathDatetime = value
-      cert.deathYear = undefined
-      this.changeSuggestionsEntry(CERT_DEATH_THIME_SUG, false)
-    } else if (value && isYear) {
-      cert.deathDatetime = value
-      cert.deathYear = (cert.deathDatetime as Date).getFullYear()
-      this.changeSuggestionsEntry(CERT_DEATH_THIME_SUG, false)
-    } else {
-      cert.deathDatetime = undefined
-      cert.deathYear = undefined
-      this.checkDeathDay()
-    }
-  }
-
-  checkSNILS() {
-    const person = this._cert.patient.person
-    const isSNILS =
-      (person.SNILS === undefined || person.SNILS.length < 14) &&
-      person.nullFlavors().findIndex((element) => element.parent_attr === "SNILS") === -1
-    this.changeSuggestionsEntry(SNILS_SUG, isSNILS)
-  }
-  setSNILS(snils: string | undefined) {
-    const person = this._cert.patient.person
-    person.SNILS = snils
-    this.checkSNILS()
-  }
-  checkOMS() {
-    const oms = this._cert.policyOMS
-    const isOMS =
-      (oms === undefined || oms.length < 7) &&
-      this._cert.nullFlavors().findIndex((element) => element.parent_attr === "policy_OMS") === -1
-    this.changeSuggestionsEntry(OMS_SUG, isOMS)
-  }
-  setPolicyOMS(oms: string | undefined) {
-    this._cert.policyOMS = oms
-    this.checkOMS()
-  }
-  setIORGCODE(code: string | undefined) {
-    const identity = this._cert.patient.identity
-    if (identity) identity.issueOrgCode = code
-    this.checkIORGCODE()
-  }
-  checkIORGCODE() {
-    const code = this._cert.patient.identity?.issueOrgCode
-    const isCODE =
-      (code === undefined || code.length < 1) &&
-      this._cert.nullFlavors().findIndex((element) => element.parent_attr === "policy_OMS") === -1
-    this.changeSuggestionsEntry(IORGCODE_SUG, isCODE)
-  }
-
-  checkIdentity() {
-    this.checkIORGDate()
-    this.checkIORGCODE()
-    this.checkIORGNAME()
-    this.checkIDNumber()
-    this.checkIDSeries()
-  }
-  checkIORGDate() {
-    const patient = this._cert.patient
-    const isIORGDate =
-      patient.identity?.issueOrgDate === undefined &&
-      patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1
-    this.changeSuggestionsEntry(IORGDATE_SUG, isIORGDate)
-  }
-  setIORGDate(date: Date | undefined) {
-    const identity = this._cert.patient.identity
-    if (identity) identity.issueOrgDate = date
-    this.checkIORGDate()
-  }
-  checkIORGNAME() {
-    const patient = this._cert.patient
-    const isIORGName =
-      (patient.identity?.issueOrgName === undefined || patient.identity?.issueOrgName.length < 15) &&
-      patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1
-    this.changeSuggestionsEntry(IORGNAME_SUG, isIORGName)
-  }
-  setIORGNAME(name: string | undefined) {
-    const identity = this._cert.patient.identity
-    if (identity) identity.issueOrgName = name
-    this.checkIORGNAME()
-  }
-  checkIDSeries() {
-    const patient = this._cert.patient
-    const isIDSeries =
-      (patient.identity?.series === undefined || patient.identity?.series.length < 1) &&
-      patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1
-    this.changeSuggestionsEntry(IDSERIES_SUG, isIDSeries)
-  }
-  setIDSeries(series: string | undefined) {
-    const identity = this._cert.patient.identity
-    if (identity) identity.series = series
-    this.checkIDSeries()
-  }
-  checkIDNumber() {
-    const patient = this._cert.patient
-    const isIDNumber =
-      (patient.identity?.number === undefined || patient.identity?.number.length < 1) &&
-      patient.nullFlavors().findIndex((element) => element.parent_attr === "identity") === -1
-    this.changeSuggestionsEntry(IDNUMBER_SUG, isIDNumber)
-  }
-  setIDNumber(number: string | undefined) {
-    const identity = this._cert.patient.identity
-    if (identity) identity.number = number
-    this.checkIDNumber()
-  }
-  checkLifeAreaType() {
-    const isLifeAreaType =
-      this._cert.lifeAreaType === undefined &&
-      this._cert.nullFlavors().findIndex((element) => element.parent_attr === "life_area_type") === -1
-    this.changeSuggestionsEntry(LIFE_AREA_SUG, isLifeAreaType)
-  }
-
-  checkLifeArea() {
-    const isLifeArea =
-      (this._cert.patient.address === undefined ||
-        this._cert.patient.address.aoGUID === undefined ||
-        this._cert.patient.address.postalCode === undefined ||
-        this._cert.patient.address.housenum === undefined) &&
-      this._cert.patient.nullFlavors().findIndex((element) => element.parent_attr === "addr") === -1
-    this.changeSuggestionsEntry(LIFE_PLACE_SUG, isLifeArea)
-  }
-
-  checkDeathAreaType() {
-    const isDeathAreaType =
-      this._cert.deathAreaType === undefined &&
-      this._cert.nullFlavors().findIndex((element) => element.parent_attr === "death_area_type") === -1
-    this.changeSuggestionsEntry(DEATH_AREA_SUG, isDeathAreaType)
-  }
-
-  checkDeathArea() {
-    const isDeathArea =
-      (this._cert.deathAddr === undefined ||
-        this._cert.deathAddr.aoGUID === undefined ||
-        this._cert.deathAddr.postalCode === undefined ||
-        this._cert.deathAddr.housenum === undefined) &&
-      this._cert.nullFlavors().findIndex((element) => element.parent_attr === "death_addr") === -1
-    this.changeSuggestionsEntry(DEATH_PLACE_SUG, isDeathArea)
-  }
-
   get suggestions() {
     return this._suggestions
   }
@@ -283,17 +292,16 @@ export default class CertificateStore {
     this._suggestions = suggestions
   }
 
-  changeSuggestionsEntry(index: number, value: boolean) {
-    if (value === this.suggestions[index].done) {
-      const nSuggestions = [...this.suggestions]
-      nSuggestions[index].done = !value
-      this._suggestions = nSuggestions
-    }
-  }
   redSuggestionsCount() {
     return this.suggestions.reduce((result, item) => {
       if (!item.done) ++result
       return result
     }, 0)
+  }
+
+  dispose() {
+    // So, to avoid subtle memory issues, always call the
+    // disposers when the reactions are no longer needed.
+    this.disposers.forEach((disposer) => disposer())
   }
 }
