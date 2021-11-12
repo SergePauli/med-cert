19046c7421b4c15bc29c1437f52ee4e533c6844a
SergePauli <SergePauli@yandex.ru>
2021-11-12 14:38:03 +0800

    feature:(add procedures[] array to IDeathReason in models), stable

--- src/models/responses/IDeathReason.ts
+++ src/models/responses/IDeathReason.ts
@@ -1,9 +1,11 @@
 import { INullFlavorable } from "../INullFlavorable"
 import { IDiagnosis } from "./IDiagnosis"
+import { IProcedure } from "./IProcedure"
 
 export interface IDeathReason extends INullFlavorable {
   id?: string
   certificate_id: number
-  diagnosis: IDiagnosis
+  diagnosis?: IDiagnosis
   effective_time?: Date
+  procedures?: IProcedure[]
 }
