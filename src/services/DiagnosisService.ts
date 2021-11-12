cde41e4960a7dacaf7290e2faad99c48bde78228
SergePauli <SergePauli@yandex.ru>
2021-11-12 14:45:20 +0800

    feature:(add fetchMedicalServs to DiagnosisService in services), stable

--- src/services/DiagnosisService.ts
+++ src/services/DiagnosisService.ts
@@ -1,6 +1,7 @@
 import { AxiosResponse } from "axios"
 import $api, { API_URL } from "../http"
 import { IDiagnosis } from "../models/responses/IDiagnosis"
+import { IMedicalServs } from "../models/responses/IMedservs"
 
 export default class DiagnosisService {
   static async fetchDiagnoses(query: any): Promise<AxiosResponse<IDiagnosis[]>> {
@@ -8,5 +9,8 @@ export default class DiagnosisService {
   }
   static async fetchExtDiagnoses(query: any): Promise<AxiosResponse<IDiagnosis[]>> {
     return $api.post(`${API_URL}model/ExtDiagnosis/`, { q: query, offset: 0, limit: 200 })
+  }
+  static async fetchMedicalServs(query: any): Promise<AxiosResponse<IMedicalServs[]>> {
+    return $api.post(`${API_URL}model/MedicalServ/`, { q: query, offset: 0, limit: 200 })
   }
 }
