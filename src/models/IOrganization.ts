df89c88fcc1b19e153a5d120c3dc76928c0416f5
SergePauli <SergePauli@yandex.ru>
2021-12-13 08:27:20 +0800

    feature:(create IOrganization inteface in models), stable

---
+++ src/models/IOrganization.ts
@@ -0,0 +1,23 @@
+import { IContact } from "./IContact"
+import { IAddress } from "./responses/IAddress"
+
+export interface IOrganization {
+  create_date: string
+  delete_date: string | null
+  delete_reason: string | null
+  guid: string
+  id: number
+  license: string
+  license_data: string
+  modify_date: string
+  name: string | null
+  name_full: string | null
+  oid: string
+  okpo: string | null
+  organization_type: number
+  osp_oid: string | null
+  parent_id: number | null
+  parent_oid: string | null
+  address: IAddress
+  contacts: IContact[]
+}
