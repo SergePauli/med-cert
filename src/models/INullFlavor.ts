9df89442eece1bdac7745eae208bffff8b67d379
SergePauli <SergePauli@yandex.ru>
2021-12-28 16:28:50 +0800

    feature:(add function to create Nullflavors automaticaly to INullFlavor.ts),stable

--- src/models/INullFlavor.ts
+++ src/models/INullFlavor.ts
@@ -1,3 +1,5 @@
+import { DESTROY_OPTION } from "../utils/consts"
+import { UNK } from "../utils/defaults"
 import { IDestroyble } from "./IDestroyble"
 
 export interface INullFlavor extends IDestroyble {
@@ -6,10 +8,31 @@ export interface INullFlavor extends IDestroyble {
   parent_attr: string
   code: number
 }
-export const getCleanNullFlavor = (old: INullFlavor) => {
+
+//получаем чистый NullFlavor без метки удаления
+export const getCleanNullFlavor = (old: INullFlavor): INullFlavor | undefined => {
   if (old === undefined) return
   let _new = { code: old.code, parent_attr: old.parent_attr } as INullFlavor
   if (old.id) _new.id = old.id
   if (old.parent_guid) _new.parent_guid = old.parent_guid
   return _new
+}
+
+//генерируем заполнители UNK автоматически,
+//проверяя карту fields<имя поля, значение>
+//и массив заполнителей до внесения изменений: original
+export const createNullFlavors = (
+  fields: Map<string, string | null | undefined>,
+  original: INullFlavor[]
+): INullFlavor[] => {
+  let _nullFlafors = [] as INullFlavor[]
+  fields.forEach((value, key) => {
+    const _nullFlavor = original.find((item) => item.parent_attr === key)
+    if (value && _nullFlavor) _nullFlafors.push({ ..._nullFlavor, ...DESTROY_OPTION })
+    else if (!value && _nullFlavor) {
+      const candidat = getCleanNullFlavor(_nullFlavor)
+      if (candidat) _nullFlafors.push(candidat)
+    } else if (!value && !_nullFlavor) _nullFlafors.push({ code: UNK, parent_attr: key })
+  })
+  return _nullFlafors
 }
