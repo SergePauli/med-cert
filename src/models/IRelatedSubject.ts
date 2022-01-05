41743f4ed2e5b03c948ae6669c5aa8e2a608b5c7
SergePauli <SergePauli@yandex.ru>
2022-01-05 11:40:05 +0800

    fix:(add destroying and saving sapport for IRelatedSubject (данные матери умершего)), stable

--- src/models/IRelatedSubject.ts
+++ src/models/IRelatedSubject.ts
@@ -1,11 +1,14 @@
+import { IDestroyble } from "./IDestroyble"
 import { INullFlavorable } from "./INullFlavorable"
 import { IPersonName } from "./IPersonName"
 import { IAddress } from "./responses/IAddress"
 
-export interface IRelatedSubject extends INullFlavorable {
+export interface IRelatedSubject extends INullFlavorable, IDestroyble {
   id: string
   family_connection: number
   addr?: IAddress
+  addr_attributes?: IAddress
   person_name?: IPersonName
+  person_name_attributes?: IPersonName
   birthTime?: Date
 }
