72e88979d8e241f8c42e8ac7b2fa60d7b26fce41
SergePauli <SergePauli@yandex.ru>
2022-01-05 11:34:18 +0800

    fix:(add destoying and saving support to IDeathReason), stable

--- src/models/responses/IDeathReason.ts
+++ src/models/responses/IDeathReason.ts
@@ -1,11 +1,14 @@
+import { IDestroyble } from "../IDestroyble"
 import { INullFlavorable } from "../INullFlavorable"
 import { IDiagnosis } from "./IDiagnosis"
 import { IProcedure } from "./IProcedure"
 
-export interface IDeathReason extends INullFlavorable {
+export interface IDeathReason extends INullFlavorable, IDestroyble {
   id?: string
   certificate_id: number
   diagnosis?: IDiagnosis
+  diagnosis_id?: number
   effective_time?: Date
   procedures?: IProcedure[]
+  procedures_attributes?: IProcedure[]
 }
