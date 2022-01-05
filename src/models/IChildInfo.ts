d054f1350abd6f8b292fcb0bc817ed97857a296b
SergePauli <SergePauli@yandex.ru>
2022-01-05 12:53:27 +0800

    refactor:(remove cert_id from IChildInfo and add savings support), stable

--- src/models/IChildInfo.ts
+++ src/models/IChildInfo.ts
@@ -1,10 +1,11 @@
+import { IDestroyble } from "./IDestroyble"
 import { INullFlavorable } from "./INullFlavorable"
 import { IRelatedSubject } from "./IRelatedSubject"
 
-export interface IChildInfo extends INullFlavorable {
-  certificate_id: number
+export interface IChildInfo extends INullFlavorable, IDestroyble {
   term_pregnancy?: number
   weight?: number
   which_account?: number
   related_subject?: IRelatedSubject
+  related_subject_attributes?: IRelatedSubject
 }
