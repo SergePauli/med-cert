664490e0f7c6d4ca9e3db9bf3aae081badc90d85
SergePauli <SergePauli@yandex.ru>
2022-01-05 11:30:28 +0800

    refactor:(remove  parent id field, and add medical_serv_id in IProcedure interface ), stable

--- src/models/responses/IProcedure.ts
+++ src/models/responses/IProcedure.ts
@@ -1,9 +1,10 @@
+import { IDestroyble } from "../IDestroyble"
 import { IMedicalServs } from "./IMedservs"
 
-export interface IProcedure {
+export interface IProcedure extends IDestroyble {
   id?: number
-  external_reason: string
   medical_serv: IMedicalServs
+  medical_serv_id?: number
   text_value?: String
   effective_time?: Date
 }
