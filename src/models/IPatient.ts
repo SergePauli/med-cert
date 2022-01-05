1f0e9b768012bf95935721a27f914617b12e3855
SergePauli <SergePauli@yandex.ru>
2022-01-05 13:01:05 +0800

    fix:(rename organization_id, and add saving sapport to IPatient), stable

--- src/models/IPatient.ts
+++ src/models/IPatient.ts
@@ -1,18 +1,17 @@
 import IIdentity from "./IIdentity"
 import { INullFlavorable } from "./INullFlavorable"
 import { IPerson } from "./IPerson"
-import { IAddress } from "./responses/IAddress"
 
 export interface IPatient extends INullFlavorable {
   id?: string
   person: IPerson
+  person_attributes?: IPerson
   identity?: IIdentity
+  identity_attributes?: IIdentity
   gender: number | undefined
   birth_date: Date | Date[] | undefined
   birth_year: number
-  birth_month: number
-  provider_organization: string
-  addr_type: number
-  address?: IAddress
-  guid: string
+  organization_id: number
+  addr_type?: number
+  guid?: string
 }
