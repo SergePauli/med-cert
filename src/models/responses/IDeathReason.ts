a34f0ef1f17b8ff0f9bbb1c41ade7306fe6ea13c
SergePauli <SergePauli@yandex.ru>
2022-01-13 12:21:33 +0800

    fix:(using guid attribute in IDeathReason), stable

--- src/models/responses/IDeathReason.ts
+++ src/models/responses/IDeathReason.ts
@@ -5,6 +5,7 @@ import { IProcedure } from "./IProcedure"
 
 export interface IDeathReason extends INullFlavorable, IDestroyble {
   id?: string
+  guid?: string
   certificate_id: number
   diagnosis?: IDiagnosis
   diagnosis_id?: number
