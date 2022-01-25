571ed990420915da11162300ee1d86e4958cd78c
SergePauli <SergePauli@yandex.ru>
2022-01-26 07:57:11 +0800

    fix:(using guid attribute in IProcedure), stable

--- src/models/responses/IProcedure.ts
+++ src/models/responses/IProcedure.ts
@@ -1,7 +1,9 @@
+import { INullFlavorable } from "../INullFlavorable"
 import { IMedicalServs } from "./IMedservs"
 
-export interface IProcedure {
+export interface IProcedure extends INullFlavorable {
   id?: number
+  guid: string
   medical_serv: IMedicalServs
   text_value?: String
   effective_time?: string
