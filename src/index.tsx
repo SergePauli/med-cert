0166f1715897c8f6eca16cb1960054ca31fec9b2
SergePauli <SergePauli@yandex.ru>
2022-01-26 07:45:46 +0800

    feature:(using TemporaryIDStore in index.tsx), stable

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,24 @@ import App from './App'
 import AddressStore from './store/addressStore'
 import CertificateStore from './store/certificateStore'
 import LayoutStore from './store/layoutStore'
+import TemporaryIDStore from './store/temporaryIDStore'
 import UserStore from './store/userStore'
 interface IState{
   userStore: UserStore
   layoutStore: LayoutStore
   certificateStore: CertificateStore
-  addressStore: AddressStore  
+  addressStore: AddressStore 
+  temporaryIDStore: TemporaryIDStore 
 }
 const userStore = new UserStore()
 const layoutStore = new LayoutStore()
 const certificateStore = new CertificateStore()
 const addressStore = new AddressStore()
+const temporaryIDStore = new TemporaryIDStore()
 addressStore.fetchRegionOptions()
-export const Context = createContext<IState>({userStore, layoutStore, addressStore, certificateStore}) 
+export const Context = createContext<IState>({userStore, layoutStore, addressStore, certificateStore, temporaryIDStore}) 
 ReactDOM.render(  
-  <Context.Provider value={{userStore, layoutStore, addressStore, certificateStore}} >
+  <Context.Provider value={{userStore, layoutStore, addressStore, certificateStore, temporaryIDStore}} >
     <App />  
   </Context.Provider> , 
   document.getElementById('root')
