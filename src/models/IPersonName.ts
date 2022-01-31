6dcb8d987e8ea748d39d5038768492e70033af64
SergePauli <SergePauli@yandex.ru>
2022-01-31 15:30:54 +0800

    feature:(use getOneLinePersonName for create FIO string from IPersonName data ), stable

--- src/models/IPersonName.ts
+++ src/models/IPersonName.ts
@@ -4,3 +4,7 @@ export interface IPersonName {
   given_1: string // имя
   given_2?: string // отчество
 }
+export const getOneLinePersonName = (person_name: IPersonName | undefined | null): string => {
+  if (!person_name) return ""
+  else return `${person_name.family} ${person_name.given_1} ${person_name.given_2 ? person_name.given_2 : ""}`.trim()
+}
