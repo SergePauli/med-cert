51923510d8718b811fc60c1c16c501fa3a95cad3
SergePauli <SergePauli@yandex.ru>
2022-02-15 13:29:32 +0800

    fix:(changes messagin in section_7 component), stable

--- src/components/c_sections/section_7.tsx
+++ src/components/c_sections/section_7.tsx
@@ -37,8 +37,8 @@ import { InputText } from 'primereact/inputtext'
           deathReason={certificate.reasonA} certificate={certificate}
            key={`ra2_${certificate.reasonA?.effectiveTime}`} 
           onChange={(reason: DeathReason | undefined)=>{ 
-            if (reason!==certificate.reasonA ) certificate.reasonA = reason
-            layoutStore.message = { severity: 'success', summary: 'Успешно', detail: 'Причина А изменена, рекомендуется сохранить изменения', life: 3000 }
+            if (reason!==certificate.reasonA ) { certificate.reasonA = reason
+            layoutStore.message = { severity: 'success', summary: 'Успешно', detail: 'Причина А изменена, рекомендуется сохранить изменения', life: 3000 }}
            }}
           disabled checked
           fieldName='a_reason' 
